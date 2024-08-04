172bd1cecab0fc5fef1b1bc4d9e698e099b349ff
Hikmah96 <hikmatyusuf2018@gmail.com>
2024-08-04 03:50:57 +0100

    my first commit

---
+++ src/main/java/com/jendereytech/introtogit/IntrotogitApplication.java
@@ -0,0 +1,13 @@
+package com.jendereytech.introtogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrotogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrotogitApplication.class, args);
+	}
+
+}
